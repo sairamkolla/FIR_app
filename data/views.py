e5204bd10f22ad0296e45fb593b0aa468139f2c9
sairam kolla <sai.kolla24@gmail.com>
2015-12-17 11:50:17 +0000

    minor changes

--- data/views.py
+++ data/views.py
@@ -15,48 +15,51 @@ def Home(request):
 
 @api_view(['POST'])
 def AddFir(request):
-    if request.method == 'POST':
-        postdata = json.loads(request.body)
-        #check name
-        try:
-            a = postdata['name']
-        except:
-            return Response({'error':'Username cannot be empty'})
-        if a == '':
-            return Response({'error':'Username cannot be empty'})
-        try:
-            b = postdata['address']
-        except:
-            return Response({'error':'Address cannot be empty'})
-        if b == '':
-            return Response({'error':'Address cannot be empty'})
-        try:
-            c = postdata['description']
-        except:
-            return Response({'error':'Description cannot be empty'})
-        if c == '':
-            return Response({'error':'Description cannot be empty'})
-        newfir = fir(name = a, address = b, description = c)
-        newfir.save()
-        return Response({'ok':request.body})
+
+    #postdata = json.loads(request.body)
+    postdata = request.data
+
+    #check name
+    try:
+        a = postdata['name']
+    except:
+        return Response({'error':'Username cannot be empty'})
+    if a == '':
+        return Response({'error':'Username cannot be empty'})
+    try:
+        b = postdata['address']
+    except:
+        return Response({'error':'Address cannot be empty'})
+    if b == '':
+        return Response({'error':'Address cannot be empty'})
+    try:
+        c = postdata['description']
+    except:
+        return Response({'error':'Description cannot be empty'})
+    if c == '':
+        return Response({'error':'Description cannot be empty'})
+    newfir = fir(name = a, address = b, description = c)
+    newfir.save()
+    return Response({'ok':'ok'})
 
 @api_view(['POST'])
 def GetFir(request):
-    if request.method == 'POST':
-        postdata = json.loads(request.body)
-        try:
-            temp = postdata['datestring']
-        except:
-            return Response({'error':'Please Select The dates'})
-        if temp == '':
-            return Response({'error':'Please Select The dates'})
-        fromdate = temp.split('-')[0].strip()
-        todate = temp.split('-')[1].strip()
-        fromdate = converttodate(fromdate)
-        todate = converttodate(todate)
-        posts = fir.objects.filter(date__range=[fromdate,todate])
-        serailizer = FirSerializer(posts,many=True)
-        return Response(serailizer.data)
+
+    #postdata = json.loads(request.body)
+    postdata = request.data
+    try:
+        temp = postdata['datestring']
+    except:
+        return Response({'error':'Please Select The dates'})
+    if temp == '':
+        return Response({'error':'Please Select The dates'})
+    fromdate = temp.split('-')[0].strip()
+    todate = temp.split('-')[1].strip()
+    fromdate = converttodate(fromdate)
+    todate = converttodate(todate)
+    posts = fir.objects.filter(date__range=[fromdate,todate])
+    serailizer = FirSerializer(posts,many=True)
+    return Response(serailizer.data)
 
 def converttodate(ref):
     temp = ref.split('/')
